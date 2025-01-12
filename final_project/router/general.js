23508074ba45cebff9fe09fc856d98caac05ae25
xbru1 <xbrumley.edu@gmail.com>
2025-01-11 18:45:22 -0800

    Tasks 1-4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,34 +4,56 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+    // Send all books
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+// NOTE: I am having to assume that the index of the object is the 'ISBN' even though a real ISBN would have 10 or 13 digits
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    // console.log("ISBN: " + isbn);
+    return res.send(JSON.stringify(books[isbn], null, 4));
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    //Write your code here
+    console.log(books);
+    const author = req.params.author;
+    let filteredBooks = {};
+
+    for (var id in books) {
+        // Using .toLowerCase() so that this isn't case sensitive
+        // The regex replaces all spaces with underscores to make using the API much easier
+        if (books[id].author.toLowerCase().replace(/\s+/g, '_') === author.toLowerCase()) {
+            filteredBooks[id] = books[id];
+        }
+    }
+    return res.send(JSON.stringify(filteredBooks, null, 4));
+    
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Same thing as the previous
+    console.log(books);
+    const title = req.params.title;
+    let filteredBooks = {};
+
+    for (var id in books) {
+        if (books[id].title.toLowerCase().replace(/\s+/g, '_') === title.toLowerCase()) {
+            filteredBooks[id] = books[id];
+        }
+    }
+    return res.send(JSON.stringify(filteredBooks, null, 4));
 });
 
 //  Get book review
